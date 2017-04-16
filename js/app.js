e728199d401e47c92416b9bac4dec4430845648f
Matthew J Roybal <matthewjroybal@gmail.com>
2017-04-15 19:03:20 -0700

    Created a new branch since I have a MVP master branch now. Dev branch next goal is validation

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 // App
-
 $(document).ready(function() {
-	var formRate = rateSelect();
-	var formTerm = termSelect();
-	$('.loan-rate').append(formRate);
-	$('.loan-term').append(formTerm);
+	var loanCost = $('.loan-cost').closest('form-group').attr("id");
+	console.log(loanCost);
+	// Allow user to select a term of months or years
+	$('.input-group-addon-btn').on('click', function() {
+		var activeBtn = $(this).hasClass('active');
+		if (!activeBtn) {
+			$(this).toggleClass('active').siblings().toggleClass('active');
+		}
+	});
+	// Submit the form for a return
 	$('.btn-loan-go').on('click', function(event) {
 		event.preventDefault();
+		// Get the values from the form
 		var cost = $('.loan-cost').val();
 		var down = $('.loan-down').val();
 		var rate = $('.loan-rate').val();
 		var term = $('.loan-term').val();
 		var taxe = $('.loan-taxe').val();
 		var insu = $('.loan-insu').val();
-		loan(cost, down, rate, term, taxe, insu);
+		var dues = $('.loan-dues').val();
+		// If the values are empty, reject the form submission
+//		validateForm(cost, down, rate, term, taxe, insu, dues);
+		// Get a result
+		loan(cost, down, rate, term, taxe, insu, dues);
 	});
+	// Reset the form to a blank slate
 	$('.btn-loan-reset').on('click', function() {
 		$(this).reset();
 	});
--- js/loan.js
+++ js/loan.js
@@ -1,12 +1,31 @@
-function annualToDollar(num) {
-	// Convert annual amount to monthly dollars
-	var numMonthly = (num / 12);
-	var numRounded = ((Math.round(numMonthly * 100)) / 100).toFixed(2);
-	var numToString = toString(numRounded);
-	return ("$" + numRounded);
+function validateForm(inputValue) {
+	var loanCost = $('.loan-cost').attr("class");
+	var loanCost = $('.loan-cost');
+	var loanDown = $('.loan-down');
+	var loanRate = $('.loan-rate');
+	var loanTerm = $('.loan-term');
+	var loanTaxe = $('.loan-taxe');
+	var loanInsu = $('.loan-insu');
+	var loanDues - $('.loan-dues');
+	
+	var cost = $('.loan-cost').val();
+	var down = $('.loan-down').val();
+	var rate = $('.loan-rate').val();
+	var term = $('.loan-term').val();
+	var taxe = $('.loan-taxe').val();
+	var insu = $('.loan-insu').val();
+	var dues - $('.loan-dues').val();
+	.forEach(function(item) {
+		if (inputValue == "") {
+			$('.loan-' + inputName).closest('form-group').addClass('has-error');
+			return false;
+		} else {
+			$('').closest('form-group').addClass('has-success');
+		}
+	})
 }
 
-function numberToDollar(num) {
+function dollars(num) {
 	// Convert fixed amount to dollars
 	var numRounded = ((Math.round(num * 100)) / 100).toFixed(2);
 	var numToString = toString(numRounded);
@@ -14,61 +33,75 @@ function numberToDollar(num) {
 }
 
 function loan(cost, down, rate, term, taxe, insu) {
+	
+	validateForm(cost);
+	
+	// Term changed to months if years selected. 
+	// If not, no changes and label is months.
+	var termID = $('.input-group-addon-btn.active').attr('id');
+	var termLength = (term * 12);
+ 	var termLabel = " years";
+	if (termID === 'input-group-addon-months') {
+		termLength = term;
+		termLabel = " months";
+	}
+
 /* Formula: M = P * ( J / (1 - (1 + J)-N))
  * M = payment amount
  * P = principal
  * J = effective interest rate divided by # yearly payments
  * N = total number of payments
  */
+
+	var taxeMonthly = (taxe / 12);
+	var insuMonthly = (insu / 12);
 	
-  var principal = cost - down;
+  var P = cost - down;
   var J = ((rate / 100) / 12);
-	var N = - (term);
+	var N = - (termLength);
   var K = (1 + J);
   var Q = Math.pow(K,N);
 	var R = (1 - Q);
 	var S = (J / R);
-  var M = (principal * S);
-	var piti = ((taxe / 12) + (insu / 12) + M);
+  var mortgageMonthly = (P * S);
+	var pitiMonthly = (taxeMonthly + insuMonthly + mortgageMonthly);
 	
-  var payment = numberToDollar(M);
-	
-	var taxeMonthly = annualToDollar(taxe);
-	var insuMonthly = annualToDollar(insu);
-	var pitiMonthly = numberToDollar(piti);
-	var downDollar = numberToDollar(down);
-	
-	var ratePercentage = (rate + "%");
-	var termMonths = (term + " Months");
+	var pitiLoan = dollars(pitiMonthly);
+  var mortLoan = dollars(mortgageMonthly);
+  var downLoan = dollars(down);
+  var rateLoan = (rate + "%");
+	var termLoan = (termLength + termLabel);
+	var taxeLoan = dollars(taxeMonthly);
+	var insuLoan = dollars(insuMonthly);
   
   var HTML = ('<table class="table table-striped">' +
                 '<tr>' +
                   '<td>Principal, Interest, Taxes, & Insurance</td>' +
-                  '<td>' + pitiMonthly + '</td>' +
+                  '<td>' + pitiLoan + '</td>' +
                 '</tr>' +
 								'<tr>' +
                   '<td>Principal & Interest only</td>' +
-                  '<td>' + payment + '</td>' +
+                  '<td>' + mortLoan + '</td>' +
                 '</tr>' +
                 '<tr>' +
                   '<td>Down payment</td>' +
-                  '<td>' + downDollar + '</td>' +
+                  '<td>' + downLoan + '</td>' +
                 '</tr>' +
                 '<tr>' +
                   '<td>Rate</td>' +
-                  '<td>' + ratePercentage + '</td>' +
+                  '<td>' + rateLoan + '</td>' +
                 '</tr>' +
                 '<tr>' +
                   '<td>Term</td>' +
-                  '<td>' + termMonths + '</td>' +
+                  '<td>' + termLoan + '</td>' +
                 '</tr>' +
 								'<tr>' +
                   '<td>Taxes</td>' +
-                  '<td>' + taxeMonthly + '</td>' +
+                  '<td>' + taxeLoan + '</td>' +
                 '</tr>' +
 								'<tr>' +
                   '<td>Insurance</td>' +
-                  '<td>' + insuMonthly + '</td>' +
+                  '<td>' + insuLoan + '</td>' +
                 '</tr>' +
               '</table>');
    return ($('#loan-results').html(HTML));
